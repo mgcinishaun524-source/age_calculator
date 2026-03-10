669304ba39457e35745888549609d10507bda9e9
mgcinishaun524 <mgcinishaun524@gmail.com>
2026-03-10 10:31:18 +0200

    first commit

---
+++ agecalculator.py
@@ -0,0 +1,24 @@
+#name = "Shaun"
+#age = 25
+#print(name)
+#print(age)
+#count = 1
+
+#while count <= 5:
+    #print(count)
+    #count += 1
+a = input("What is your Name?\n")
+b = input("What is your Surname?\n")
+c = int(input("When were you born?\n"))
+print(a,b, "you are " + str(2026 - int(c)) + " years old")
+
+
+
+
+#for i in range (5):
+    #for j in range(5):
+        #print(i, j)
+    #print (i)
+
+
+    
